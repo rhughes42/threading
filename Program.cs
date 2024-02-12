e3c80136f0b3f1283aaa69cb35df847145fe3703
Ryan Hughes <31203021+rhughes42@users.noreply.github.com>
2024-02-12 22:09:27 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+
+// Graph Consult Aps https://graphconsult.xyz/
+
+internal class Program {
+
+	private static void Main( string[] args ) {
+		int numberOfTasks = 10000; // Number of tasks to start.
+
+		Stopwatch sw = Stopwatch.StartNew( );
+		Console.WriteLine( $"Starting {numberOfTasks} tasks in {Environment.ProcessId}." );
+
+		Task[] tasks = new Task[ numberOfTasks ];
+		ConcurrentDictionary<int, int> threadRuns = new( );
+
+		for ( int i = 0; i < numberOfTasks; i++ ) {
+			int taskNumber = i;
+			tasks[ i ] = Task.Run( ( ) => {
+				int Id = Environment.CurrentManagedThreadId;
+				Console.WriteLine( $"[{sw.ElapsedMilliseconds}ms]: Task {taskNumber} executing on {Id}." );
+
+				// If the thread has not been run before, add it to the dictionary. Otherwise,
+				// increment the count of runs for this thread.
+				threadRuns.AddOrUpdate( Id, 1, ( key, oldValue ) => oldValue + 1 );
+			} );
+		}
+
+		// Wait for all tasks to complete.
+		Task.WaitAll( tasks );
+		Console.WriteLine( $"All tasks completed after {sw.ElapsedMilliseconds}ms." );
+
+		// Return some simple statistics.
+		Console.WriteLine( $"Threads used: {threadRuns.Keys.Count}." );
+		foreach ( int key in threadRuns.Keys ) {
+			Console.WriteLine( $"Thread {key} executed {threadRuns[ key ]} tasks." );
+		}
+
+		// Calculate the frequency distribution of the threads.
+		Dictionary<int, double> freq = CalculateFrequency( threadRuns, 2 );
+		foreach ( KeyValuePair<int, double> item in freq ) {
+			Console.WriteLine( $"Thread {item.Key} handles {item.Value}% of the tasks." );
+		}
+	}
+
+	/// <summary>
+	/// Calculate the frequency distribution of a dictionary.
+	/// </summary>
+	/// <param name="dict">     </param>
+	/// <param name="rounding"> </param>
+	/// <returns> A dictionary of values representing their rate of appearance. </returns>
+	public static Dictionary<int, double> CalculateFrequency( ConcurrentDictionary<int, int> dict, int rounding ) {
+		int totalValues = dict.Values.Sum( );
+
+		Dictionary<int, double> frequencyDistibution = [];
+		foreach ( int key in dict.Keys ) {
+			// Calculate the frequency of each key and add it to the frequency distribution
+			// dictionary.
+			double percentage = Math.Round( ( double ) dict[ key ] / totalValues * 100, rounding );
+			frequencyDistibution[ key ] = percentage;
+		}
+		return frequencyDistibution;
+	}
+}
